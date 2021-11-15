2df98e2a18492f0d22e58b983a02543917036b07
Nuh Çolakkadıoğlu <n.colakkadioglu@gmail.com>
2021-11-15 04:39:28 +0300

    Update GuestPost.cs

--- GuestBook.Entities/Concrete/GuestPost.cs
+++ GuestBook.Entities/Concrete/GuestPost.cs
@@ -9,14 +9,14 @@ namespace GuestBook.Entities.Concrete
 {
     public class GuestPost:IEntity
     {
-        //test veri
+       
         public int Id { get; set; }
         public string Name { get; set; } 
         public string LastName { get; set; } 
         public string Email { get; set; }
         public string PostContent { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.Now;
-        //test data
+       
     
     }
 }
